8fac3f659ef7df1ff23932fc77d255447819c95a
chininchu <120990876+chininchu@users.noreply.github.com>
2023-08-21 12:09:37 -0500

    Fixed the .stop method

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
 "use strict";
+
+
+$(document).ready(function () {
+
+    let score = 0;
+
+    function hideMole(mole) {
+
+        mole.stop().animate({bottom: '-100px'}, 300);
+
+
+    }
+
+    function showMole() {
+
+        let holes = $('.hole');
+        let randomHole = $(holes[Math.floor(Math.random() * holes.length)]);
+
+        let mole = randomHole.find('.mole');
+
+        mole.stop().animate({bottom: '0'}, 300);
+
+        setTimeout(function () {
+            hideMole(mole);
+
+
+        }, 1000);
+
+        setTimeout(showMole, 1500);
+
+
+    }
+
+    $('.mole').click(function () {
+        let mole = $(this);
+        hideMole(mole);
+        score += 1;
+        $('#score').text(score);
+
+
+    });
+
+    setTimeout(showMole, 500);
+
+
+});
+
+
+
+
+
+
+
+
+
+
